944cfb8a8589bad072d6b39ce29e5f5c0cf0e7cd
Julian Wakefield <julianwakefield@outlook.com>
2020-02-17 09:59:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,7 +11,7 @@ end
 
 def using_pop(deleted_string, continents)
 continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-deleted_string=continents.pop!
-continents
+continents.pop
+
 deleted_string
 end
