f78d9eed06ef0daafdfd2927057521940a897f07
Julian Wakefield <julianwakefield@outlook.com>
2020-02-17 10:45:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,7 @@ end
 
 def pop_with_args(dog_breeds)
 dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-small_dogs = dog_breeds.pop(2,3)
+small_dogs = dog_breeds.pop(2)
 p small_dogs
 
 end
