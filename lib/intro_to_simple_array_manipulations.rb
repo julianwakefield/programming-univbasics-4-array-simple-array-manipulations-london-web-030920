b020706fe9c710cf5d7e4ffc2a967d85053a12c3
Julian Wakefield <julianwakefield@outlook.com>
2020-02-17 09:55:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, colors_in_the_rainbow)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array, bouroughs_in_nyc)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(deleted_string=continents.pop, continents)
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+deleted_string=continents.pop
+continents
+deleted_string
+end
