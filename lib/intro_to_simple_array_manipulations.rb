f02ebc61518220e916892b05838257f51227a932
Julian Wakefield <julianwakefield@outlook.com>
2020-02-17 14:49:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -57,8 +57,8 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer)
+def using_delete_at(array)
 
-  array.delete(3)
+  array.delete("Robocop")
   
 end
